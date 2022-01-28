950c163c256036cc6c9ff44dc82c41a63b905229
jfabrice2019 <jfbayoro@gmail.com>
2022-01-28 08:28:43 -0600

    fifth commit

--- src/main/java/com/availityBackend/demo/controller/UserController.java
+++ src/main/java/com/availityBackend/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.availityBackend.demo.model.User;
 import com.availityBackend.demo.service.UserService;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UserController {
 	
